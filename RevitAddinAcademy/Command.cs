ceb4048fd6447af882debf754e9b362c7a6ebf00
Mladen Varbanov <varbanov.architecture@gmail.com>
2022-06-30 11:32:17 +0300

    sync

--- RevitAddinAcademy/Command.cs
+++ RevitAddinAcademy/Command.cs
@@ -25,32 +25,9 @@ namespace RevitAddinAcademy
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            // Access current selection
+            TaskDialog.Show("Hello", "This is my first command add-in");
+            TaskDialog.Show("Hello again", "This is another window");
 
-            Selection sel = uidoc.Selection;
-
-            // Retrieve elements from database
-
-            FilteredElementCollector col
-              = new FilteredElementCollector(doc)
-                .WhereElementIsNotElementType()
-                .OfCategory(BuiltInCategory.INVALID)
-                .OfClass(typeof(Wall));
-
-            // Filtered element collector is iterable
-
-            foreach (Element e in col)
-            {
-                Debug.Print(e.Name);
-            }
-
-            // Modify document within a transaction
-
-            using (Transaction tx = new Transaction(doc))
-            {
-                tx.Start("Transaction Name");
-                tx.Commit();
-            }
 
             return Result.Succeeded;
         }
